77945f19086c086889f198dbd4ed42dbf7ab6d1c
Fajar Ihsana <67202122+Zynfinity@users.noreply.github.com>
2023-11-14 14:45:55 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 <table border='1' width='100%' height='100' cellspacing='0' cellpadding='0'>
-<tr><td colspan='2' height='150' align='center'><img src='./header2.png' width='100%' height='150'</td></tr>
+<tr><td colspan='2' height='150' align='center'><img src='https://images.unsplash.com/photo-1699755094450-620e32ec86a6?q=80&w=1636&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' width='100%' height='150'</td></tr>
 <tr><td valign='top' bgcolor='#FFF0F5'>
 <table bgcolor='#FAF0E6' border='2' width='100%' cellspacing='0' cellpadding='5'>
 	<tr>
